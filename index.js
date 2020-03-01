ccd6e64315b87763f4f6e352ea8c6f0ba422cfc0
Tess Fagan <tessfagan@gmail.com git config --global user.name Tess>
2020-02-29 19:19:05 -0500

    stop icon and stop function

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 window.SpeechRecognition = window.webkitSpeechRecognition || window.SpeechRecognition;
 const synth = window.speechSynthesis;
-const recognition = new SpeechRecognition();
+const recognition = new SpeechRecognition.continuous();
+recognition = true;
 
 const icon = document.querySelector('i.fa.fa-microphone')
+const stop = document.querySelector('i."fa fa-stop')
 let paragraph = document.createElement('p');
 let container = document.querySelector('.text-box');
 container.appendChild(paragraph);
@@ -13,28 +15,28 @@ icon.addEventListener('click', () => {
     dictate();
 });
 
-// icon.addEventListener('click', ()=> {
-
-// })
+stop.addEventListener('click', () => {
+    endDictate();
+})
 
 const dictate = () => {
     recognition.start();
+    // recognition.onresult = (event) => {
+    //     const speechToText = event.results[0][0].transcript;
+
+    //     console.log(event.results[0])
+
+    //     paragraph.textContent = speechToText;
+
+    // }
+}
+
+const endDictate = () => {
+    recognition.stop();
     recognition.onresult = (event) => {
         const speechToText = event.results[0][0].transcript;
 
-        console.log(event.results[0])
-
         paragraph.textContent = speechToText;
-
     }
+
 }
-
-// const endDictate = () => {
-//     recognition.stop();
-//     recognition.onresult = (event) => {
-//         const speechToText = event.results[0][0].transcript;
-
-//         paragraph.textContent = speechToText;
-//     }
-
-// }
